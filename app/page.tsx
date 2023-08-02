00879031c72c417b76e81ba4001c1b5dd0925971
Rajat6697 <rajatverma5338@gmail.com>
2023-08-03 00:21:45 +0530

    layout created for page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
+import Trending from "app/(home)/Trending";
 
 
 export default function Home() {
+
+  
+
   return (
-    <main>
+    <main className="px-10 leading-7">
+    
+    <Trending/>
+
+    <div className="md:flex  gap-10 mb-5">
+      <div className="basis-3/4">
+        {/* <Tech/> */}
+        {/* <Travel/> */}
+        {/* <Other/> */}
+        <div className="hidden md:block">
+        {/* <Subscribe/> */}
+      </div>
+      </div>
       
 
-      Hiiiii
+    <div className="basis-1/4">
+
+    </div>
+
+    </div>
 
   
     </main>
