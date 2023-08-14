a9f462c2ca65449d09833e747c26634ae85a2aef
Rajat6697 <rajatverma5338@gmail.com>
2023-08-14 21:33:23 +0530

    Card component final

--- app/(shared)/Card.tsx
+++ app/(shared)/Card.tsx
@@ -23,6 +23,21 @@ const Card = ({className, imageHeight, isSmallCard = false, isLongForm = false}:
             ${isSmallCard ? "line-clamp-2" : ""}
             `}>title</h4>
         </Link>
+        <div  className={`${isSmallCard ? "my-2" : "flex my-3" } gap-3`}>
+
+            <h5 className='font-semibold text-xs'>
+                author 
+            </h5>
+            <h6 className='text-wh-300 text xs'>
+                date
+            </h6>
+
+        </div>
+        <p className={`text-wh-500 ${isLongForm ? "line-clamp-5" : "line-clamp-3"}`}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut earum at nam veritatis dicta. Aspernatur eveniet sit eum, saepe molestiae quidem repudiandae neque expedita quis. Deleniti placeat exercitationem voluptatibus ipsum.
+        Officia est sed aspernatur fuga ad dolore, nihil voluptas accusamus eaque eos aliquid iusto ducimus fugiat! At tempore nostrum, alias, beatae quo ipsa veniam error, excepturi sunt nemo ducimus tenetur!
+        Omnis ratione facere voluptatum, sed esse eum eaque totam facilis nobis hic recusandae, delectus beatae reiciendis eius tempora accusamus at aut aspernatur vero ab. Quod alias deserunt maxime a aut.
+        </p>
         </div>
     </div>
   )
