c88a22e9fd13129f5c753d53cd260a9c8889a1fa
Rajat6697 <rajatverma5338@gmail.com>
2023-08-15 00:43:47 +0530

    Subscribe component added

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Trending from "app/(home)/Trending";
 import Tech from "app/(home)/Tech";
 import Travel from "app/(home)/Travel";
 import Other from "app/(shared)/Other";
+import Subscribe from "app/(shared)/Subscribe";
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
           <Tech />
           <Travel/>
           <Other/>
-          <div className="hidden md:block">{/* <Subscribe/> */}</div>
+          <div className="hidden md:block"><Subscribe/></div>
         </div>
 
         <div className="basis-1/4"></div>
