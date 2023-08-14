f85bbdc6a81bb748acd116753cd619c8060aa7ea
Rajat6697 <rajatverma5338@gmail.com>
2023-08-14 22:02:30 +0530

    Other section final layout

--- app/(shared)/Other.tsx
+++ app/(shared)/Other.tsx
@@ -1,10 +1,43 @@
 import React from 'react'
+import Card from './Card'
 
 type Props = {}
 
 const Other = (props: Props) => {
   return (
-    <div>Other</div>
+    <section>
+            <hr className='border-1'/>
+         {/* <Header/> */}
+         <p className="font-bold text-2xl my-8">Other Trending Posts</p>
+   
+        <div className='sm:grid grid-cols-2 gap-16'>
+        <Card className=" bg-wh-500 mt-5 sm:mt-0" 
+        isLongForm
+        imageHeight="h-80"
+        />
+        <Card className=" bg-wh-500 mt-5 sm:mt-0" 
+        isLongForm
+        imageHeight="h-80"
+        />
+        <Card className=" bg-wh-500 mt-5 sm:mt-0" 
+        isLongForm
+        imageHeight="h-80"
+        />
+        <Card className=" bg-wh-500 mt-5 sm:mt-0" 
+        isLongForm
+        imageHeight="h-80"
+        />
+        <Card className=" bg-wh-500 mt-5 sm:mt-0" 
+        isLongForm
+        imageHeight="h-80"
+        />
+        <Card className=" bg-wh-500 mt-5 sm:mt-0" 
+        isLongForm
+        imageHeight="h-80"
+        />
+        </div>
+
+    </section>
   )
 }
 
